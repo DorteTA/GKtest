1944e7f3cbce34a9d9450b36a15aa44107e3d936
DorteTA <dorteta@gmail.com>
2015-03-10 14:12:47 +0100

    dortesindex.php

---
+++ dortesindex.php
@@ -0,0 +1,36 @@
+<?php
+
+$header = <<<END
+<!DOCTYPE html>
+<html>
+
+	<head>
+		<title>GamersKeep - Keepet m flest flasksvaler </title>
+		<link rel="stylesheet" href=css/style.css>
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css">
+	</head>
+
+	<body>
+		<div id="header">
+			<div id="container">
+				<div class="row">
+					<div class="col-md-12">Dorte was here!</div>
+				</div>
+			</div>
+		</div>
+END;
+
+$content = <<<END
+	<div>
+	Dorte is in da house!
+	</div>
+
+	</body>
+</html>
+END;
+
+echo $header;
+
+echo $content;
+
+?>
